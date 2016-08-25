be8b1622f00cccf71d7cae54db797c6573436671
justin leggett <justin@codingrookie.com>
2016-08-25 13:39:43 -0500

    completed the assignment

--- songs.js
+++ songs.js
@@ -24,15 +24,58 @@ for(var i = 0; i < songs.length; i++) {
 // 3. Students must find and replace the > character in each item with a - character.
     songs[i] = songs[i].replace(/>/g,"-");
 
-// Add each song to the div 1 at a time
-    var styledSong = songs.length[i] =  '<h3>' + songs[i]+ '</h3>';
-    document.getElementById("album").innerHTML += styledSong;
+    displaySong(songs[i]);
+}
 
+function displaySong(song) {
+    var styledSong = '<h3>' + song + '</h3>';
+    album.innerHTML += styledSong;
+}
+
+function addSongToArrayandDisplay(songName, artistName, albumName) {
+    songs.push(songName + " - by " + artistName + " on the album " + albumName);
+    displaySong(songName + " - by " + artistName + " on the album " + albumName);
+}
+
+function clearInputFields(songNameInput, artistName, albumName) {
+    songNameInput.value = "";
+    artistName.value = "";
+    albumName.value = "";
+}
+
+function postSong() {
+    var songNameInput = document.getElementById("songNameInput");
+    var artistName = document.getElementById("artistName");
+    var albumName = document.getElementById("albumName");
+    addSongToArrayandDisplay(songNameInput.value, artistName.value, albumName.value);
+    clearInputFields(songNameInput, artistName, albumName);
 }
 
 
+function showHidden(event) {
+    event.preventDefault();
+    var setVis = document.getElementById("setVisibility");
+    var songName = document.getElementById("songName");
+    var addMusicView = document.getElementById("addMusicView");
+    // if visHidden is in className
+    if(addMusicView.className.indexOf("visHidden") > -1) {
+        setVis.classList.add("visHidden");
+        songName.classList.add("visHidden");
+        addMusicView.classList.remove("visHidden");
+    } else {
+        setVis.classList.remove("visHidden");
+        songName.classList.remove("visHidden");
+        addMusicView.classList.add("visHidden");
+        postSong();
+    }
+}
+
+// When "Add Music" is clicked hide sidebar and song_name and make visible add_music_view
+document.getElementById("addMusic").addEventListener("click", showHidden)
 
 
+// When add is clicked add the song to the array of songs
+document.getElementById("addSongButton").addEventListener("click", showHidden)
 
 
 
