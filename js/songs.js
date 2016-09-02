efdba79284eb89ff3bc10c49baa4fa918c2acfb9
justin leggett <justin@codingrookie.com>
2016-09-02 15:42:56 -0500

    technically it works, but I still need to clean up code and change event:isteners

--- js/songs.js
+++ js/songs.js
@@ -1,44 +1,39 @@
 var album = document.getElementById("album");
+var songs = {};
+var songs1 = {};
 
 // Students must use JavaScript to create arrays, modify them (using only the following methods:
 // join, push, reverse, sort, concat and unshift), and output lists to the DOM.
 // Songs array
-var songs = ["Diamonds", "Let Love Win", "Transformed", "God Is On The Move", "Dear Younger Me", "Evidence", "Made New", "Tase The Feeling", "You Are I am", "I Can Only Image"];
+// var songs = ["Diamonds", "Let Love Win", "Transformed", "God Is On The Move", "Dear Younger Me", "Evidence", "Made New", "Tase The Feeling", "You Are I am", "I Can Only Image"];
 
-// Artist array
-var artist = ["Hawk Nelson", "Carrollton", "Shonlock", "7eventh Time Down", "MercyMe", "Citizen Way", "Lincoln Brewster", "Conrad Sewell", "Mariah Carey", "Kallie Leggett"];
+// // Artist array
+// var artist = ["Hawk Nelson", "Carrollton", "Shonlock", "7eventh Time Down", "MercyMe", "Citizen Way", "Lincoln Brewster", "Conrad Sewell", "Mariah Carey", "Kallie Leggett"];
 
-// Albums array
-var albums = ["muse Sick-N-Hour Mess Age", "B-Day", "Who Will Cut Our Hair When We're Gone?", "Miss E...", "Big Willie Style", "Chocolate Factory",
- "The Spaghetti Incident", "Me, I Am Mariah", "Kisses On The Bottom", "Allow Us To Be Frank"];
+// // Albums array
+// var albums = ["muse Sick-N-Hour Mess Age", "B-Day", "Who Will Cut Our Hair When We're Gone?", "Miss E...", "Big Willie Style", "Chocolate Factory",
+//  "The Spaghetti Incident", "Me, I Am Mariah", "Kisses On The Bottom", "Allow Us To Be Frank"];
 
 // Clear the id album
 album.innerHTML = "";
 
-// 1. Each student must add one song to the beginning and the end of the array.
-// add to last position of the array
-// songs.push("The Logical Song > by Justin Leggett on the album Breakfast in Murfreesboro");
+// // 2. Loop over the array and remove any words or characters that obviously don't belong.
+// for(var i = 0; i < songs.length; i++) {
+//     songs[i] = songs[i].replace(/[* @ ( ) !]/g," ");
 
-// add to the front of the array
-// songs.unshift("The Logical Song > by Katie Leggett on the album Breakfast in Murfreesboro");
+// // 3. Students must find and replace the > character in each item with a - character.
+//     songs[i] = songs[i].replace(/>/g,"-");
 
-// 2. Loop over the array and remove any words or characters that obviously don't belong.
-for(var i = 0; i < songs.length; i++) {
-    songs[i] = songs[i].replace(/[* @ ( ) !]/g," ");
-
-// 3. Students must find and replace the > character in each item with a - character.
-    songs[i] = songs[i].replace(/>/g,"-");
-
-    displaySong(album, songs[i] + " - by " + artist[i] + " on the album " + albums[i] );
-}
+//     displaySong(album, songs[i] + " - by " + artist[i] + " on the album " + albums[i] );
+// }
 
 function displaySong(outputField, song) {
-    var styledSong = '<h3>' + song + '</h3>';
+    var styledSong = '<h3>' + '<input type="button" class="delete" value="Delete"> ' + song + '</h3>';
     outputField.innerHTML += styledSong;
 }
 
 function addSongToArrayandDisplay(songName, artistName, albumName) {
-    songs.push(songName + " - by " + artistName + " on the album " + albumName);
+    // songs.push(songName + " - by " + artistName + " on the album " + albumName);
     displaySong(album, songName + " - by " + artistName + " on the album " + albumName);
 }
 
@@ -82,8 +77,59 @@ document.getElementById("addMusic").addEventListener("click", showHidden)
 // When add is clicked add the song to the array of songs
 document.getElementById("addSongButton").addEventListener("click", showHidden)
 
+// When "Delete" button is clicked remove element from the dom
+document.getElementById("album").addEventListener("click", function(e) { //.getElementsByTagName("input")
+    e.target.parentNode.remove();
+})
 
+// var x = document.getElementById("album").getElementsByTagName("input");
+// console.log("x.length = ", x.length);
+// console.log("x = ", x);
+// document.getElementsByClassName("delete").forEach(function() {
+//     console.log("test");
+// });
+// console.log(all.length);
+// for(var i = 0; i < all.length; i++)
+// {
+//     alert(all[i].innerHTML);
+// }
+// When "More" is clicked add additional songs
+document.getElementById("more").addEventListener("click", function() {
+    // console.log(songs1);
+    formatSongs(songs1);
+})
 
+function onLoad() {
+    songs = JSON.parse(this.responseText);
+    formatSongs(songs);
+}
+
+function onLoadOne() {
+    songs1 = JSON.parse(this.responseText);
+}
+
+function onError() {
+  console.log("An error occurred while transferring");
+}
+
+function formatSongs(songsArray) {
+    for(var i = 0; i < songsArray.songs.length; i++) {
+        displaySong(album, songsArray.songs[i].title + " - by " + songsArray.songs[i].artist + " on the album " + songsArray.songs[i].album);
+    }
+}
+
+var songsRequest = new XMLHttpRequest();
+songsRequest.addEventListener("load", onLoad);
+songsRequest.addEventListener("error", onError)
+songsRequest.open("GET", "json/songs.json")
+songsRequest.send();
+
+var songs1Request = new XMLHttpRequest();
+// fix before submitting
+songs1Request.addEventListener("load", onLoadOne);
+songs1Request.addEventListener("error", onError)
+songs1Request.open("GET", "json/songs1.json")
+songs1Request.send();
 
 
 
